8d8639ba9d7deb19cdf7f7f3cb6cbe91fcfcb3ce
Marcelo Lara <mlarac7@gmail.com>
2026-05-25 09:35:59 -0400

    arreglo de dicomviewer y descarga del zip

--- routes/dicomRoutes.js
+++ routes/dicomRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { downloadDicom, viewDicom, listDicomFiles } from '../controllers/dicomController.js';
+import { serveDicom, viewDicom, listDicomFiles, downloadStudy } from '../controllers/dicomController.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
 import auditMiddleware from '../middlewares/auditMiddleware.js';
 
@@ -10,6 +10,8 @@ router.use(isAuthenticated);
 
 router.get('/visualizar/:studyId', auditMiddleware('Visualización'), viewDicom);
 router.get('/archivos/:studyId', auditMiddleware('Listar Archivos DICOM'), listDicomFiles);
-router.get('/descargar/:studyId/:filename', auditMiddleware('Descarga'), downloadDicom);
+router.get('/archivos/:studyId/:filename', auditMiddleware('Lectura Archivo DICOM'), serveDicom);
+router.get('/descargar/:studyId', auditMiddleware('Descarga de Estudio Completo'), downloadStudy);
+router.get('/descargar-zip/:studyId', auditMiddleware('Descarga de Estudio Completo'), downloadStudy);
 
 export default router;
